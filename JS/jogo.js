e421933fe2bcc40241a240b909bcf4cf0c038638
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-27 11:17:35 +0100

    Update jogo.js

--- JS/jogo.js
+++ JS/jogo.js
@@ -7,22 +7,48 @@ function ajustaTamanhoTela(){
     largura = window.innerWidth;
     altura = window.innerHeight;
 
-    console.log(altura, largura);
+    console.log(largura, altura);
 
     // apesar de já definida, só vai redimencionar atraves do evento onresize
 }
 ajustaTamanhoTela();
 
-var posicaoX = Math.floor(Math.random() * largura); // ajustar os valores randomicos no intervalo da largura
-var posicaoY = Math.floor(Math.random() * altura); // ajustar os valores randomicos no intervalo da altura
+function posicaoRandomica(){
+
+
+var posicaoX = Math.floor(Math.random() * largura) - 90; // ajustar os valores randomicos no intervalo da largura! estamos ubtraindo 90 pq a rolagem ainda acontece pq a imagem vale 50px
+var posicaoY = Math.floor(Math.random() * altura) - 90; // ajustar os valores randomicos no intervalo da altura
 console.log(posicaoX, posicaoY);
+
+  posicaoX = posicaoX < 0 ? 0 : posicaoX; // operador ternario para controlar a posição
+  posicaoY = posicaoY < 0 ? 0 : posicaoY; // operador ternario para controlar a posição
 
 //criar o html
 var mosquito = document.createElement('img');
 mosquito.src = 'imagens/mosquito.png';
-mosquito.className = 'mosquito1'; // atribuindo a classe css
+mosquito.className = tamanhoAleatorio(); // atribuindo a classe css
 //atribuir aos elementos os valores randomicos
 mosquito.style.left = posicaoX+'px';
-mosquito.style.left = posicaoX+'px';
+mosquito.style.top = posicaoY+'px';
 mosquito.style.position = 'absolute';
-document.body.appendChild(mosquito)// atribuindo o filho(imagem) no elemento pai(body)
+document.body.appendChild(mosquito);// atribuindo o filho(imagem) no elemento pai(body)
+
+tamanhoAleatorio();
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3); // classes aleatorias para alternar o tamanho do mosquito
+    console.log(classe);
+
+    switch(classe){
+        case 0:
+            return 'mosquito1';
+        
+        case 1:
+            return 'mosquito2';
+            
+        case 2:
+            return 'mosquito3';
+    }
+
+}
