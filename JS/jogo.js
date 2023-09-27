c2fef6c1fc09f1dfe534a9335cfd317eb2e246b8
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-27 10:29:37 +0100

    Update jogo.js

--- JS/jogo.js
+++ JS/jogo.js
@@ -12,3 +12,7 @@ function ajustaTamanhoTela(){
     // apesar de já definida, só vai redimencionar atraves do evento onresize
 }
 ajustaTamanhoTela();
+
+var posicaoX = Math.floor(Math.random() * largura); // ajustar os valores randomicos no intervalo da largura
+var posicaoY = Math.floor(Math.random() * altura); // ajustar os valores randomicos no intervalo da altura
+console.log(posicaoX, posicaoY);
